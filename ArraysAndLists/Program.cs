26fe4c9211fc0b493b0c15c87924afe85fab3ad8
Andrew Sutton <ajsutton94@gmail.com>
2020-06-25 13:42:35 -0500

    finished the assignment, then separated the loop placement into methods

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ArraysAndLists
 {
@@ -9,11 +12,15 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            var numberOneToTen = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
+            var evens = new List<int>();
+            var odds = new List<int>();
+
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -21,12 +28,51 @@ namespace ArraysAndLists
              * Then add those numbers to either the evens List
              * or the odds List
              */
+            // SEE METHODS BELOW
+            evens = PutInEvens(numberOneToTen);
+            odds = PutInOdds(numberOneToTen);
 
-            /* Now using foeach or for loops,
+            /* Now using foreach or for loops,
              * display each List of even and odd numbers
              *
              * Try to be creative in your display
              */
+            Console.WriteLine("All of you even numbers are:");
+            foreach(var number in evens)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("All of you odd numbers are:");
+            foreach (var number in odds)
+            {
+                Console.Write(number + " ");
+            }
+        }
+        public static List<int> PutInEvens(int[] input)
+        {
+            var listOfEvens = new List<int>();
+            foreach (var item in input)
+            {
+                if (item % 2 == 0)
+                {
+                    listOfEvens.Add(item);
+                }
+            }
+            return listOfEvens;
+        }
+        public static List<int> PutInOdds(int[] input)
+        {
+            var listOfOdds = new List<int>();
+            foreach (var item in input)
+            {
+                if (item % 2 != 0)
+                {
+                    listOfOdds.Add(item);
+                }
+            }
+            return listOfOdds;
         }
     }
+   
 }
